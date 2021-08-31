543505c281b9930cd9b4d2483ef7c93d52ad96a7
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2021-08-31 18:07:34 +0200

    Added MinMax game functionality.

--- src/se/iths/java21/patrik/lab1/MinMax.java
+++ src/se/iths/java21/patrik/lab1/MinMax.java
@@ -1,22 +1,5 @@
 package se.iths.java21.patrik.lab1;
 
-/*
-Alternativet Min Max ska fungera ungefär som uppgift 2 men nu ska vi istället
-läsa in 5 heltal och när inläsningen är klar ska vi skriva ut det minsta och det
-största talet av de inlästa. Även negativa tal ska kunna hanteras.
-Uppgiften kan lösas antingen med hjälp av en array som lagrar talen som läses in
-eller så lagrar man bara det hittills största och minsta talet i två variabler.
-(En lösning som använder 5 variabler är inte godkänd.)
-        Exempel på inläsningen
-        1
-        2
-        10
-        -2
-        100
-        Min value: -2
-        Max value: 100
-*/
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
